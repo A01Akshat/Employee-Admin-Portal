88e5675895e17df2a0ccbe3481e5a6476ff731ab
Akshat Srivastava <104577005+A01Akshat@users.noreply.github.com>
2025-02-04 18:19:38 +0530

    Add files via upload

---
+++ Models/AddEmployeeDto.cs
@@ -0,0 +1,14 @@
+﻿
+namespace EmployeeAdminPortal.Models
+{
+    public class AddEmployeeDto
+    {
+        public required string Name { get; set; }
+
+        public required string Email { get; set; }
+
+        public string? Phone { get; set; } //? to say that its a nullable property
+
+        public decimal Salary { get; set; }
+    }
+}
---
+++ Models/Entities/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace EmployeeAdminPortal.Models.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }  //global unique identifier
+        public required string Name { get; set; }
+
+        public required string Email { get; set; }
+
+        public string? Phone { get; set; } //? to say that its a nullable property
+
+        public decimal Salary { get; set; }
+
+    }
+}
